c5070866090147b1dabe3c4b3ad506b910e8422e
rabin0623 <rana_rabin@yahoo.com>
2015-11-16 19:53:05 -0700

    Beautify the javascript files after code cleanup

--- js/SettingsScreen.js
+++ js/SettingsScreen.js
@@ -1,76 +1,66 @@
-var Settings = (function()
-{
-	//settings screen variables
-	var frequency ;
-	var totalObservation;
-	
-	//default values
-	const DEFAULT_FREQUENCY = 15;
-	const DEFAULT_TOTAL_TIME = 15;
-	
-	function Settings()
-	{
-		frequency = DEFAULT_FREQUENCY;
-		totalObservation = DEFAULT_TOTAL_TIME;
-	}
-	
-	Settings.prototype.getFrequency = function()
-	{
-		return frequency;
-	}
-	
-	Settings.prototype.getTotalObservationTime = function()
-	{
-		return totalObservation;
-	}
-	
-	//using this method for debugging purposes
-	Settings.prototype.show = function()
-	{	
-		console.log("Selected frequency is :"+frequency);
-		console.log("Selected total time is :"+totalObservation);
-	}
-	
-	function getSelectedValue(elementId)
-	{
-		var temp;
-		var selectedValue;
-		temp = document.getElementById(elementId);
-		selectedValue = temp.options[temp.selectedIndex].value;
-		
-		return selectedValue;
-	}
-	
-	Settings.prototype.reset = function()
-	{
-		totalObservation = 15;
-		if(document.getElementById("frequency").disabled == false)
-		{
-			frequency = 15;
-			document.getElementById("frequency").value = frequency;
-		}
+var Settings = (function() {
+    //settings screen variables
+    var frequency;
+    var totalObservation;
 
-		
-		document.getElementById("totalTime").value = totalObservation;
-	}
-	
-	Settings.prototype.cancel = function()
-	{
-		document.getElementById("frequency").value = frequency;
-		document.getElementById("totalTime").value = totalObservation;
-	}
-	
-	Settings.prototype.save = function()
-	{	
-		frequency = getSelectedValue("frequency");
-		totalObservation = getSelectedValue("totalTime");
-		
-		document.getElementById("frequency").value = frequency;
-		document.getElementById("totalTime").value = totalObservation;
-		
-	}
-	
-	return Settings;
+    //default values
+    const DEFAULT_FREQUENCY = 15;
+    const DEFAULT_TOTAL_TIME = 15;
+
+    function Settings() {
+        frequency = DEFAULT_FREQUENCY;
+        totalObservation = DEFAULT_TOTAL_TIME;
+    }
+
+    Settings.prototype.getFrequency = function() {
+        return frequency;
+    }
+
+    Settings.prototype.getTotalObservationTime = function() {
+        return totalObservation;
+    }
+
+    //using this method for debugging purposes
+    Settings.prototype.show = function() {
+        console.log("Selected frequency is :" + frequency);
+        console.log("Selected total time is :" + totalObservation);
+    }
+
+    function getSelectedValue(elementId) {
+        var temp;
+        var selectedValue;
+        temp = document.getElementById(elementId);
+        selectedValue = temp.options[temp.selectedIndex].value;
+
+        return selectedValue;
+    }
+
+    Settings.prototype.reset = function() {
+        totalObservation = 15;
+        if (document.getElementById("frequency").disabled == false) {
+            frequency = 15;
+            document.getElementById("frequency").value = frequency;
+        }
+
+
+        document.getElementById("totalTime").value = totalObservation;
+    }
+
+    Settings.prototype.cancel = function() {
+        document.getElementById("frequency").value = frequency;
+        document.getElementById("totalTime").value = totalObservation;
+    }
+
+    Settings.prototype.save = function() {
+        frequency = getSelectedValue("frequency");
+        totalObservation = getSelectedValue("totalTime");
+
+        document.getElementById("frequency").value = frequency;
+        document.getElementById("totalTime").value = totalObservation;
+
+    }
+
+    return Settings;
 }());
 
 var modalSettings = new Settings();
--- js/timer.js
+++ js/timer.js
@@ -1,200 +1,174 @@
-var Timer = (function()
-{
-	//variables to run the timer
-	var time;
-	var clocktimer;
-	var startTime;
-	var elapsedTime;
+var Timer = (function() {
+    //variables to run the timer
+    var time;
+    var clocktimer;
+    var startTime;
+    var elapsedTime;
 
-	//variables keeping track of stop watch usage
-	var numberOfPauses;
-	var observationStartTime;
-	var observationEndTime;
+    //variables keeping track of stop watch usage
+    var numberOfPauses;
+    var observationStartTime;
+    var observationEndTime;
 
-function Timer()
-	{
-		startTime = 0;
-		elapsedTime = 0;
-		observationEndTime = 0;
-		observationStartTime = 0;
-		numberOfPauses = 0;
-	}
-	
-//get the start time for timer
-function getStartValue()
-	{
-		startTime = new Date();
-		
-		//recording the observation start time. Happens only once in the observation screen.
-		setObservationStartTime(startTime);
+    function Timer() {
+        startTime = 0;
+        elapsedTime = 0;
+        observationEndTime = 0;
+        observationStartTime = 0;
+        numberOfPauses = 0;
+    }
 
-		startTime = startTime.getTime();
-	
-		return startTime;
-	}
+    //get the start time for timer
+    function getStartValue() {
+        startTime = new Date();
 
-//get the current time 
-function currentTime()
-	{
-		return elapsedTime + (startTime ? (new Date()).getTime() - startTime: 0);
-		
-	}
-	
-function setObservationStartTime(recordedStartTime)
-	{
-		if(!observationStartTime)
-		{
-			observationStartTime = recordedStartTime;
-			console.log("START TIME: "+observationStartTime);
-		}
-	}
+        //recording the observation start time. Happens only once in the observation screen.
+        setObservationStartTime(startTime);
 
-Timer.prototype.getObservationStartTime = function()
-{
-		document.getElementById("startTime").innerHTML = observationStartTime;
-		return observationStartTime;
-}
-	
-function update()
-	{
-	//when startTime is not zero meaning that start has been pressed once in a row
-	if(startTime !=0)
-			{
-				
-				document.getElementById("timer").innerHTML = formatTime(currentTime());
-				console.log("elapsedTime "+formatTime(currentTime()));
-			}
+        startTime = startTime.getTime();
 
-	}
-	
-Timer.prototype.start = function(limit)
-	{
-		//this code needs to be changed at some point
-		var temp = limit *1000;
-		console.log("limit: "+ temp);
-		
-			//toggle between start and pause
-			if(document.getElementById("startButton").value == "Start")
-			{
-				getStartValue();
-				
-				clocktimer = setInterval(function()
-				{
-						if(startTime !=0 && currentTime()<temp)
-						{
-				
-							document.getElementById("timer").innerHTML = formatTime(currentTime());
-							console.log("elapsedTime "+formatTime(currentTime()));
-						}
-						else
-						{
-							observationTimer.stop();
-						}
-						
-				}
-				, 0.9);
-				
-				document.getElementById("startButton").value = "Pause";
-				document.getElementById("startButton").style.background = '#FFFF00';
-				document.getElementById("settingsButton").disabled = true;
-				document.querySelector(".settingsButton").style.opacity = 0.6;
-				
-				console.log("Start button pressed");
-				
-			}
-			else
-			{	
-				pause();
-			}
-	
-	}
+        return startTime;
+    }
+
+    //get the current time 
+    function currentTime() {
+        return elapsedTime + (startTime ? (new Date()).getTime() - startTime : 0);
+
+    }
+
+    function setObservationStartTime(recordedStartTime) {
+        if (!observationStartTime) {
+            observationStartTime = recordedStartTime;
+            console.log("START TIME: " + observationStartTime);
+        }
+    }
+
+    Timer.prototype.getObservationStartTime = function() {
+        document.getElementById("startTime").innerHTML = observationStartTime;
+        return observationStartTime;
+    }
+
+    function update() {
+        //when startTime is not zero meaning that start has been pressed once in a row
+        if (startTime != 0) {
+
+            document.getElementById("timer").innerHTML = formatTime(currentTime());
+            console.log("elapsedTime " + formatTime(currentTime()));
+        }
+
+    }
+
+    Timer.prototype.start = function(limit) {
+        //this code needs to be changed at some point
+        var temp = limit * 1000;
+        console.log("limit: " + temp);
+
+        //toggle between start and pause
+        if (document.getElementById("startButton").value == "Start") {
+            getStartValue();
+
+            clocktimer = setInterval(function() {
+                if (startTime != 0 && currentTime() < temp) {
+
+                    document.getElementById("timer").innerHTML = formatTime(currentTime());
+                    console.log("elapsedTime " + formatTime(currentTime()));
+                } else {
+                    observationTimer.stop();
+                }
+
+            }, 0.9);
+
+            document.getElementById("startButton").value = "Pause";
+            document.getElementById("startButton").style.background = '#FFFF00';
+            document.getElementById("settingsButton").disabled = true;
+            document.querySelector(".settingsButton").style.opacity = 0.6;
+
+            console.log("Start button pressed");
+
+        } else {
+            pause();
+        }
+
+    }
 
 
-function pause()
-{			
-	numberOfPauses++;
-				
-	elapsedTime = startTime ? elapsedTime + (new Date()).getTime() - startTime : 0;
-	startTime = 0;
-				
-	clearInterval(clocktimer);
-				
-	changeUI("Start", '#00CC33', true, 0.6, false, 1);
-	console.log("Paused button pressed"+numberOfPauses);
-}
+    function pause() {
+        numberOfPauses++;
 
-function setObservationEndTime()
-{
-	if(!observationEndTime)
-	{
-		observationEndTime = new Date();
-		console.log("STOP TIME: "+observationEndTime);
-	}
-}
+        elapsedTime = startTime ? elapsedTime + (new Date()).getTime() - startTime : 0;
+        startTime = 0;
 
-Timer.prototype.getObservationEndTime = function()
-{
-	document.getElementById("endTime").innerHTML = observationEndTime;
-	return observationEndTime;
-}
+        clearInterval(clocktimer);
+
+        changeUI("Start", '#00CC33', true, 0.6, false, 1);
+        console.log("Paused button pressed" + numberOfPauses);
+    }
+
+    function setObservationEndTime() {
+        if (!observationEndTime) {
+            observationEndTime = new Date();
+            console.log("STOP TIME: " + observationEndTime);
+        }
+    }
+
+    Timer.prototype.getObservationEndTime = function() {
+        document.getElementById("endTime").innerHTML = observationEndTime;
+        return observationEndTime;
+    }
 
 
-Timer.prototype.stop = function()
-	{
-			startTime = 0;
-			elapsedTime = 0;
-			numberOfPauses = 0;
-			
-			//recording the observation end time. Happens only once in the observation screen.
-			setObservationEndTime();
-			
-			clearInterval(clocktimer);
-			
-			document.getElementById("timer").innerHTML = "00:00:00";
-			
-			changeUI("Start", "#00CC33", false, 1, false, 1);
-			console.log("stopping timer");
-	}
+    Timer.prototype.stop = function() {
+        startTime = 0;
+        elapsedTime = 0;
+        numberOfPauses = 0;
 
-function pad (num, size)
-{
-			var s = "0000" + num;
-			return s.substr(s.length - size);	
-}
+        //recording the observation end time. Happens only once in the observation screen.
+        setObservationEndTime();
 
-function formatTime(time)
-{
-		var h = m = s = ms = 0;
-		var newTime = '';
-		
-		h = Math.floor( time / (60 * 60 * 1000) );
-		time = time % (60 * 60 * 1000);
-		m = Math.floor( time / (60 * 1000) );
-		time = time % (60 * 1000);
-		s = Math.floor( time / 1000 );
-		ms = time % 1000;
+        clearInterval(clocktimer);
 
-		
-		newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2);
-		return newTime;
-		<!-- return time; -->
-}
+        document.getElementById("timer").innerHTML = "00:00:00";
+
+        changeUI("Start", "#00CC33", false, 1, false, 1);
+        console.log("stopping timer");
+    }
+
+    function pad(num, size) {
+        var s = "0000" + num;
+        return s.substr(s.length - size);
+    }
+
+    function formatTime(time) {
+        var h = m = s = ms = 0;
+        var newTime = '';
+
+        h = Math.floor(time / (60 * 60 * 1000));
+        time = time % (60 * 60 * 1000);
+        m = Math.floor(time / (60 * 1000));
+        time = time % (60 * 1000);
+        s = Math.floor(time / 1000);
+        ms = time % 1000;
 
 
-function changeUI(buttonText, buttonColor, frequencyEnabled, frequencyOpacity, settingsEnabled, settingsOpacity)
-{
-			document.getElementById("startButton").value = buttonText;
-			document.getElementById("startButton").style.background = buttonColor;
-			
-			document.getElementById("frequency").disabled = frequencyEnabled;
-			document.querySelector(".modal-body-frequency").style.opacity = frequencyOpacity;
-			
-			document.getElementById("settingsButton").disabled = settingsEnabled;
-			document.querySelector(".settingsButton").style.opacity = settingsOpacity;
-	
-}
+        newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2);
+        return newTime;
+        <!-- return time; -->
+    }
 
-	return Timer;
+
+    function changeUI(buttonText, buttonColor, frequencyEnabled, frequencyOpacity, settingsEnabled, settingsOpacity) {
+        document.getElementById("startButton").value = buttonText;
+        document.getElementById("startButton").style.background = buttonColor;
+
+        document.getElementById("frequency").disabled = frequencyEnabled;
+        document.querySelector(".modal-body-frequency").style.opacity = frequencyOpacity;
+
+        document.getElementById("settingsButton").disabled = settingsEnabled;
+        document.querySelector(".settingsButton").style.opacity = settingsOpacity;
+
+    }
+
+    return Timer;
 }());
 
 var observationTimer = new Timer();
