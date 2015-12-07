b2f13bcdfd6d52800ae5906fe10b51f4101fbe6b
Daniel Malmer <dmalmer@gmail.com>
2015-12-07 14:18:34 -0700

    Cleanup code

--- src/js/StudentInfoController.js
+++ src/js/StudentInfoController.js
@@ -78,30 +78,6 @@ var StudentInfoController = (function() {
     }
     
     function setLocalStorageFields() {
-        studentID = ((document.getElementById('siID').value != '') ? document.getElementById('siID').value : null);
-        studentName = ((document.getElementById('siStudentName').value != '') ? document.getElementById('siStudentName').value : null);
-        dateOfBirth = ((document.getElementById('siDOB').value != '') ? document.getElementById('siDOB').value : null);
-        consentDate = ((document.getElementById('siConsentDate').value != '') ? document.getElementById('siConsentDate').value : null);
-        ethnicity = ((document.getElementById('siEthnicity').value != '') ? document.getElementById('siEthnicity').value : null);
-        primaryLanguage = ((document.getElementById('siPrimaryLanguage').value != '') ? document.getElementById('siPrimaryLanguage').value : null);
-        gradeLevel = ((document.getElementById('siGradeLevel').value != '') ? document.getElementById('siGradeLevel').value : null);
-        teacherName = ((document.getElementById('siTeacherName').value != '') ? document.getElementById('siTeacherName').value : null);
-        currClass = ((document.getElementById('siClass').value != '') ? document.getElementById('siClass').value : null);
-        observerName = ((document.getElementById('siObserverName').value != '') ? document.getElementById('siObserverName').value : null);
-        observerTitle = ((document.getElementById('siObserverTitle').value != '') ? document.getElementById('siObserverTitle').value : null);
-        
-        localStorage.setItem("studentID", studentID);
-        localStorage.setItem("studentName", studentName);
-        localStorage.setItem("dateOfBirth", dateOfBirth);
-        localStorage.setItem("consentDate", consentDate);
-        localStorage.setItem("ethnicity", ethnicity);
-        localStorage.setItem("primaryLanguage", primaryLanguage);
-        localStorage.setItem("gradeLevel", gradeLevel);
-        localStorage.setItem("teacherName", teacherName);
-        localStorage.setItem("currClass", currClass);
-        localStorage.setItem("observerName", observerName);
-        localStorage.setItem("observerTitle", observerTitle);
-        
         localStorage.setItem("studentID", document.getElementById('siID').value);
         localStorage.setItem("studentName", document.getElementById('siStudentName').value);
         localStorage.setItem("dateOfBirth", document.getElementById('siDOB').value);
@@ -113,6 +89,13 @@ var StudentInfoController = (function() {
         localStorage.setItem("currClass", document.getElementById('siClass').value);
         localStorage.setItem("observerName", document.getElementById('siObserverName').value);
         localStorage.setItem("observerTitle", document.getElementById('siObserverTitle').value);
+
+                
+        console.log('studentID = ' + localStorage.getItem('studentID') + '.');
+
+        
+        
+
     }
 
     return StudentInfoController;
